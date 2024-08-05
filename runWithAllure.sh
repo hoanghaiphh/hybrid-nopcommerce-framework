9524b7be63d8a1b4dbd464ef9a85cee5066cd462
hoanghaiphh <hoanghai.phh@gmail.com>
2024-08-05 23:31:00 +0700

    Update runWithAllure.sh for running on MacOS/Linux: - Make the file executable - double click to run - Replace absolute path with relative path - Add code to close Terminal after finish

--- runWithAllure.sh
+++ runWithAllure.sh
@@ -1,5 +1,7 @@
-project_path="/Users/MAC/Automation/03 Java Hybrid Framework/hybrid-nopcommerce-framework"
+called_path=${0%/*}
+project_path=${called_path#[^/]*}
 cd "$project_path"
 java -javaagent:"$project_path/libraries/allure/aspectjweaver-1.9.22.1.jar" -classpath "$project_path/out/production/hybrid-nopcommerce-framework:$project_path/libraries/allure/*:$project_path/libraries/extentReports/*:$project_path/libraries/log4j-reportng/*:$project_path/libraries/selenium-testng/*" org.testng.TestNG "$project_path/resources/allure.xml"
 source ~/.bash_profile
 allure serve ./allure-results/
+kill `ps -A | grep -w Terminal.app | grep -v grep | awk '{print $1}'`

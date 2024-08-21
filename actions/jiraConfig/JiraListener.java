2ee25f330d944adcaa43a5975d627714e5a269f1
Hoang Hai <hoanghai.phh@gmail.com>
2024-08-21 16:25:48 +0700

    Update: - Integrate Jira - Change site to localhost

--- actions/jiraConfig/JiraListener.java
+++ actions/jiraConfig/JiraListener.java
@@ -15,7 +15,7 @@ public class JiraListener implements ITestListener {
             JiraServiceProvider JiraServiceProvider = new JiraServiceProvider(
                     GlobalConstants.JIRA_URL,
                     GlobalConstants.JIRA_USERNAME,
-                    GlobalConstants.JIRA_API_KEY,
+                    "ATATT3xFfGF010Vhfk0OQbqYc_mTQ7kXRHCV274zacfD5mtKL8jQTsy-Rh4O2d9INd5NDc0tQyEU66EAVx_V22Qrp2mo0JFrBewxSr5Gv8OjMkuC8zLWNBMmxKjOnWFa0qOdxAXC-WngMubeI4qopil-Q4Z2MYbpUupWL1Mp5BIQzJwOUsyK9Cg=FCC69E42",
                     GlobalConstants.JIRA_PROJECT_KEY);
             String issueDescription = "Failure Reason from Automation Testing\n\n" + result.getThrowable().getMessage() + "\n";
             issueDescription.concat(ExceptionUtils.getFullStackTrace(result.getThrowable()));
